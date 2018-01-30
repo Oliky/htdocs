665a51c0fde41a2cfb1a883f2bba49d143b88ea6
oliwerkrzyszczyk <oliwerkrzyszczyj@googlemail.com>
2018-01-30 09:59:45 +0100

    Fixed Problem

---
+++ School/login.php
@@ -0,0 +1,27 @@
+
+<html>
+<head>
+
+    <link href="css/scrolling-nav.css" rel="scrolling-nav.css">
+
+</head>
+<body>
+
+
+<div class="container">
+    <form action="index.html" method="post">
+        <div class="form-input">
+            <input type="text" name="username" >
+        </div>
+        <div class="form-input">
+            <input type="text" name="password" >
+        </div>
+            <input type="submit" value="Einloggen">
+        </div>
+    </form>
+</div>
+
+</body>
+
+</html>
+
